f74d94dbe7e09ae3a9d9f666247388cb3e9b5528
Makarand30 <158295754+Makarand30@users.noreply.github.com>
2025-01-07 23:13:23 +0100

    Add files via upload

---
+++ Business Q1.sql
@@ -0,0 +1,9 @@
+SELECT 
+    city_id,
+    COUNT(trip_id) AS total_trips,
+    ROUND(AVG(fare_amount / `distance_travelled(km)`), 2) AS avg_fare_per_km,
+    ROUND(AVG(fare_amount), 2) AS avg_fare_per_trip,
+    ROUND((COUNT(trip_id) * 100.0 / SUM(COUNT(trip_id)) OVER ()), 2) AS percent_contribution_to_total_trips
+FROM trips_db.fact_trips
+GROUP BY city_id
+;
