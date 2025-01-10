3bacebf0d5780bd67f6a78116a0efd1919025eac
Makarand30 <158295754+Makarand30@users.noreply.github.com>
2025-01-10 02:40:34 +0100

    Update Business Q6.2.sql

--- Business Q6.2.sql
+++ Business Q6.2.sql
@@ -1,8 +1,10 @@
+---Business Request - 6: Repeat Passenger Rate Analysis---
+---City-wide Repeat Passenger Rate---
 SELECT
     city_id,
     SUM(repeat_passengers) AS total_repeat_passengers,
     SUM(total_passengers) AS total_passengers,
-    ROUND(SUM(repeat_passengers) * 100.0 / SUM(total_passengers), 2) AS city_repeat_passenger_rate
+    ROUND(SUM(repeat_passengers) * 100.0 / SUM(total_passengers), 2) AS city_repeat_passenger_rate --- Calculating RPR by city in percentage
 FROM
     fact_passenger_summary
 GROUP BY
