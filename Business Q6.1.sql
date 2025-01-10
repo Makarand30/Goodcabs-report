2456414435f1595cd281ce1e411b6f5d794166f9
Makarand30 <158295754+Makarand30@users.noreply.github.com>
2025-01-10 02:38:41 +0100

    Update Business Q6.1.sql

--- Business Q6.1.sql
+++ Business Q6.1.sql
@@ -1,10 +1,12 @@
+--Business Request - 6: Repeat Passenger Rate Analysis--
+--1.	Monthly Repeat Passenger Rate----
 WITH fact_passenger_summary AS (
     SELECT 
         city_id,
         `Month Name` AS month_name,
         SUM(repeat_passengers) AS total_repeat_passengers,
         SUM(total_passengers) AS total_passengers,
-        ROUND(SUM(repeat_passengers) * 100.0 / SUM(total_passengers), 2) AS monthly_repeat_passenger_rate
+        ROUND(SUM(repeat_passengers) * 100.0 / SUM(total_passengers), 2) AS monthly_repeat_passenger_rate---- calculating RPR in percentage
     FROM 
         fact_passenger_summary
     GROUP BY 
